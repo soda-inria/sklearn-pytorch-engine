8d6b69d9d39a00118b847259afeaf29df4fad331
Olivier Grisel <olivier.grisel@ensta.org>
2023-09-20 17:45:24 +0200

    Fix has_fp64_support for MPS on Apple Silicon

--- sklearn_pytorch_engine/_utils.py
+++ sklearn_pytorch_engine/_utils.py
@@ -35,6 +35,16 @@ def has_fp64_support(device):
     try:
         torch.zeros(1, dtype=torch.float64, device=device)
         return True
-    except RuntimeError as runtime_error:
-        if "data type is unsupported" in str(runtime_error):
+    except RuntimeError as e:
+        if "data type is unsupported" in str(e):
             return False
+        raise
+    except TypeError as e:
+        # On Apple Silicon M1 with the MPS device, the following error is
+        # raised:
+        #
+        # TypeError: Cannot convert a MPS Tensor to float64 dtype as the MPS
+        # framework doesn't support float64. Please use float32 instead.
+        if "doesn't support float64" in str(e):
+            return False
+        raise
